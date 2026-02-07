9c1801c91cb3b901c606acd88bbf070f94341ccc
jfsicilia <jfsicilia@gmail.com>
2026-02-07 11:05:41 +0000

    Added autogen use cases. Better comments. Almost final versions of all framework files

--- .config/kanata/scripts/kanata_sync_apps.py
+++ .config/kanata/scripts/kanata_sync_apps.py
@@ -13,21 +13,35 @@ ACTIONS_FOLDER = KANATA_FOLDER / "actions"
 ACTIONS_FILE = ACTIONS_FOLDER / "actions.kbd"
 ACTION_PREFIX = "action_"
 ACTIONS_PREFIX = "actions_"
-ACTION_START_RE = re.compile(r"^\s*(action_[^\s]+)\s.*switch")
+AUTOGEN_ACTION_RE = re.compile(r"^\s*(!action_[^\s]+|action_[^\s]+)\s.*@autogen@.*")
+REVERSE_ACTION_FLAG = "!"
 ACTION_END = ")"
 APP_ACTION_RE = re.compile(r"^\s*(\w+)_action_(.+?)\s")
 
-AUTOGEN_SECTION_RE = re.compile(r";;\s+@@")
+AUTOGEN_SECTION_RE = re.compile(r";;\s+@autogen@")
 
 VK_LINE_RE = re.compile(r"\(\(input virtual vk_")
 
+FILENAME_RE = re.compile(
+    rf"^{re.escape(ACTIONS_PREFIX)}([^.]+)(?:\.(\d+))?\.{re.escape(KANATA_EXT)}$"
+)
+
 
 def find_apps():
-    apps = []
+    files_with_order = []
+
     for p in ACTIONS_FOLDER.glob(f"{ACTIONS_PREFIX}*.{KANATA_EXT}"):
-        name = p.stem[len(ACTIONS_PREFIX) :]
-        apps.append(name)
-    return sorted(apps)
+        m = FILENAME_RE.match(p.name)
+        if m:
+            app_name = m.group(1)
+            order_str = m.group(2)
+            order = int(order_str) if order_str else None
+            files_with_order.append((order, app_name, p.name))
+
+    files_with_order.sort(key=lambda x: x[0] if x[0] is not None else 0)
+
+    # devuelve solo los nombres de app
+    return [(app_name, app_file) for _, app_name, app_file in files_with_order]
 
 
 def autogen_kanata_file(lines, apps):
@@ -45,42 +59,46 @@ def autogen_kanata_file(lines, apps):
             "\n;; Apps Virtual Keys =====================================================\n"
         )
         out.append("(defvirtualkeys\n")
-        for app in apps:
-            out.append(f"  vk_{app:<12} XX\n")
+        for app_name, _ in apps:
+            out.append(f"  vk_{app_name:<12} XX\n")
         out.append(")\n\n")
 
         out.append(
             ";; Actions & Apps' Actions ===============================================\n"
         )
-        out.append(f"(include {ACTIONS_FILE.relative_to(KANATA_FOLDER)})\n")
-        for app in apps:
+        # out.append(f"(include {ACTIONS_FILE.relative_to(KANATA_FOLDER)})\n")
+        for _, app_file in apps:
             out.append(
-                f"(include {ACTIONS_FOLDER.relative_to(KANATA_FOLDER)}/{ACTIONS_PREFIX}{app}.{KANATA_EXT})\n"
+                f"(include {ACTIONS_FOLDER.relative_to(KANATA_FOLDER)}/{app_file})\n"
             )
         break
 
     return out
 
 
-def load_app_actions(app):
+def load_app_actions(app_name, app_file):
     f"""
     Returns a set of action names (without '{ACTIONS_PREFIX}' prefix)
     """
     actions = set()
-    path = ACTIONS_FOLDER / f"{ACTIONS_PREFIX}{app}.{KANATA_EXT}"
+    path = ACTIONS_FOLDER / f"{app_file}"
     if not path.exists():
         return actions
 
     for line in path.read_text().splitlines():
         m = APP_ACTION_RE.match(line.strip())
-        if m and m.group(1) == app:
+        if m and m.group(1) == app_name:
             actions.add(m.group(2))
     return actions
 
 
 def sync_actions():
-    apps = find_apps()
-    app_actions = {app: load_app_actions(app) for app in apps}
+    non_reversed_apps = find_apps()
+    reversed_apps = non_reversed_apps[::-1]
+    app_actions = {
+        app_name: load_app_actions(app_name, app_file)
+        for app_name, app_file in non_reversed_apps
+    }
 
     lines = ACTIONS_FILE.read_text().splitlines(keepends=True)
     out = []
@@ -88,16 +106,23 @@ def sync_actions():
     i = 0
     while i < len(lines):
         line = lines[i]
-        m = ACTION_START_RE.match(line)
 
+        m = AUTOGEN_ACTION_RE.match(line)
         if not m:
             out.append(line)
             i += 1
             continue
 
-        # ---- Start of an action block ----
-        action_name = m.group(1)  # action_lctl+b
-        short_name = action_name[len(ACTION_PREFIX) :]  # lctl+b
+        # ---- Process autogen of an action block ----
+        action_name = m.group(1)  # action_lctl+b or !action_lctl+b
+        if action_name.startswith(REVERSE_ACTION_FLAG):
+            short_name = action_name[
+                len(REVERSE_ACTION_FLAG) + len(ACTION_PREFIX) :
+            ]  # lctl+b
+            apps = reversed_apps
+        else:
+            short_name = action_name[len(ACTION_PREFIX) :]  # lctl+b
+            apps = non_reversed_apps
 
         out.append(line)
         i += 1
@@ -107,20 +132,20 @@ def sync_actions():
             i += 1
 
         # Insert regenerated per-app bindings
-        for app in apps:
-            # print(f"{app} find {short_name}")
-            if short_name in app_actions[app]:
+        for app_name, _ in apps:
+            if short_name in app_actions[app_name]:
                 out.append(
-                    f"    ((input virtual vk_{app})) ${app}_{ACTION_PREFIX}{short_name} break\n"
+                    f"    ((input virtual vk_{app_name})) ${app_name}_{ACTION_PREFIX}{short_name} break\n"
                 )
 
-        # Copy default line: () XX break / () _ break
-        while i < len(lines) and lines[i].strip().startswith(ACTION_END):
+        # Copy all until action definition ends line. It usually copies
+        # default binding of action:  () XX break / () _ break
+        while i < len(lines):
             out.append(lines[i])
+            line = lines[i]
             i += 1
-        # ACTION_END
-        out.append(lines[i])
-        i += 1
+            if line.strip().startswith(ACTION_END):
+                break
 
     return out
 
@@ -182,22 +207,3 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     main(force=args.force)
-
-# def main():
-#     apps = find_apps()
-#
-#     if not apps:
-#         raise RuntimeError(
-#             f"No {ACTIONS_PREFIX}<app>.{KANATA_EXT} files found in {ACTIONS_FOLDER}"
-#         )
-#
-#     text = KANATA_FILE.read_text().splitlines(keepends=True)
-#     result = autogen_kanata_file(text, apps)
-#     KANATA_FILE.write_text("".join(result), encoding="utf-8")
-#
-#     result = sync_actions()
-#     ACTIONS_FILE.write_text("".join(result), encoding="utf-8")
-#
-#
-# if __name__ == "__main__":
-#     main()
